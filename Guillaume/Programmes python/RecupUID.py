3058b9bf4aca83f06b015850a6c4b4fce3f87f0d
Monvoisin Guillaume <guillaume.monvoisin@stfelixlasalle.fr>
2020-04-10 17:44:57 +0200

    Ajout du programme python+des librairies+une capture de la 1°ère erreur

---
+++ Guillaume/Programmes python/RecupUID.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2.7
+#-- coding: utf-8 --
+
+import RPi.GPIO as GPIO
+from pirc522 import RFID
+import time
+
+GPIO.setwarnings(False)
+
+rc522 = RFID()
+
+while True:
+    print (1)
+    rc522.wait_for_tag()
+    print (2)
+    (error,tag_type) = rc522.request()
+    if not error:
+        (error,uid) = rc522.anticoll()
+        if not error:
+            print("UID:" + str(uid))
+            time.sleep(1)
